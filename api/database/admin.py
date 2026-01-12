bba2a72a96096b3b1f11c546137eee449c71b9e7
rachel-soon <rachel_may0106@hotmail.com>
2026-01-12 23:31:58 +0800

    chore; register whiteboard model in admin panel

--- api/database/admin.py
+++ api/database/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Whiteboard
+
 
 # Register your models here.
+@admin.register(Whiteboard)
+class WhiteboardAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "description", "created_at", "updated_at"]
+    search_fields = ["name", "description"]
+    list_filter = ["created_at", "updated_at"]
+    readonly_fields = ["created_at", "updated_at"]
